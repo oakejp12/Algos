a9f7f86ccac77e5f7d41cf4858f4f5f11a02fb9e
Johan Oakes <johanoakes@gmail.com>
2019-03-27 22:57:29 -0400

    Add methods to reverse a graph for algorithms utility, and to print a graph for logging.

--- DynamicProgramming/ExchangeRates/Graph.py
+++ DynamicProgramming/ExchangeRates/Graph.py
@@ -1,4 +1,7 @@
 
+from Vertex import Vertex
+
+
 class Graph:
 
     def __init__(self):
@@ -9,7 +12,7 @@ class Graph:
         return iter(self.vertices.values())
 
     def add_vertex(self, node):
-        self.num_vertices = self.num_vertices + 1
+        self.num_vertices += 1
 
         new_vertex = Vertex(node)
 
@@ -23,20 +26,53 @@ class Graph:
         else:
             return None
 
-    def add_edge(self, fromNode, toNode, weight=0: int):
+    def add_edge(self, fromNode, toNode, weight=0):
         if fromNode not in self.vertices:
             self.add_vertex(fromNode)
         if toNode not in self.vertices:
             self.add_vertex(toNode)
 
         self.vertices[fromNode].add_neighbor(toNode, weight)
-        self.vertices[toNode].add_neighbor(fromNode, weight)
 
     def get_vertices(self):
-        return self.vert_dict.keys()
+        return self.vertices.keys()
 
     def set_previous(self, current):
         self.previous = current
 
-    def get_previous(self, current):
+    def get_previous(pasself, current):
         return self.previous
+
+    def __str__(self):
+        string = ""
+        V = set(self.get_vertices())
+        for node in V:
+            vertex = self.get_vertex(node)
+            string = string + vertex.__str__() + '\n'
+        return string
+
+    @staticmethod
+    def reverse_graph(G):
+        '''
+        Reverse the graph so that out-going edges become in-going edges
+        Useful for the Bellman-Ford algorithm
+        '''
+
+        G_new = Graph()
+
+        V = set(G.get_vertices())
+
+        # For each vertex in G, we'll grab their neighbors
+        # Add the neighbors as a new vertex (if they don't exist)
+        # to the new graph G_new, then add the source vertex as
+        # a neighbor to the neighbors
+        for node in V:
+            sourceVertex = G.get_vertex(node)
+            neighbors = set(sourceVertex.get_connections())
+            for neighbor in neighbors:
+                if G_new.get_vertex(neighbor) is None:
+                    G_new.add_vertex(neighbor)
+                weightFromSource = sourceVertex.get_weight(neighbor)
+                G_new.add_edge(neighbor, node, weightFromSource)
+
+        return G_new
